0e388cb37c757d6db044594ead3b897868f1a35e
Genesis <gevaldez@csumb.edu>
2018-02-12 05:40:03 +0000

    Add all lab2 files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+    include 'inc/functions.php';
+?>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    
+<div id="main">
+    <body>
+        <?php
+            play();
+        ?>
+    <form>
+        <input type="submit" value="Spin!"/>
+    </form>
+        
+    </body>
+</div>
+</html>
